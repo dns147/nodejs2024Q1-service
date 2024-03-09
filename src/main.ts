e7507c6e7e632eaf7f722c595640d17a89f8da9a
dns147 <rarden@ya.ru>
2024-03-09 14:39:32 +0300

    feat: add OpenAPI spec from api.yaml file

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import 'dotenv/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { readFileSync } from 'fs';
+import { parse } from 'yaml';
+import { serve, setup } from 'swagger-ui-express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT;
+
+  //const config = new DocumentBuilder()
+    // .setTitle('Service')
+    // .setDescription('REST Service')
+    // .setVersion('1.0')
+    // .addTag('Service')
+    // .build();
+
+  //const document = SwaggerModule.createDocument(app, config);
   
+  const file = readFileSync('./doc/api.yaml', 'utf8');
+  const swaggerDocument = parse(file);
+
+  //SwaggerModule.setup('api', app, document);
+
+  app.use('/api-docs', serve, setup(swaggerDocument));
+
   await app.listen(port);
 
   console.log(`Server is listening http://localhost:${port}`);
+  console.log(`You can use swagger http://localhost:${port}/api-docs`);
 }
 
 bootstrap();
