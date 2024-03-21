84a18887f296d808828a4bc9d6109aeaf1212bfa
dns147 <rarden@ya.ru>
2024-03-22 00:18:50 +0300

    feat: add prisma module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { TrackModule } from './modules/track/track.module';
 import { AlbumModule } from './modules/album/album.module';
 import { ArtistModule } from './modules/artist/artist.module';
 import { FavoriteModule } from './modules/favorite/favorite.module';
+import { PrismaModule } from './modules/prisma/prisma.module';
 
 @Module({
-  imports: [UserModule, TrackModule, AlbumModule, ArtistModule, FavoriteModule],
+  imports: [UserModule, TrackModule, AlbumModule, ArtistModule, FavoriteModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
